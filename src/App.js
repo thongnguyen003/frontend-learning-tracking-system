70463ce09f2a132efd84b32ae89b6be646f0ad03
HuuHau235 <hhai98940@gmail.com>
2025-05-07 21:17:33 +0700

    feature/10 - Done for develop student information edit feature

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StudentHomepage from './pages/StudentHomepage';
+import StudentProfile from './pages/StudentProfile';
 function App() {
   return (
       <Router>
           <Routes>
             {/* Cập nhật cách khai báo Route với element */}
-            <Route path="/" element={<StudentHomepage />} />
+            {/* <Route path="/" element={<StudentHomepage />} /> */}
+            <Route path="/" element={<StudentProfile />} />
           </Routes>
       </Router>
   );
