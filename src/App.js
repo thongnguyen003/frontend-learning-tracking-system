4762528620215aaa68bddda3515b04285bb3eafb
unknown <xomdangvaisf@gmail.com>
2025-05-19 21:15:43 +0700

    Done Naviga teacher to their homepage after login

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StudentHomepage from './features/student/pages/StudentHomepage';
 import PortfolioProfile from './pages/ProfilePage';
-import Login from './features/Auth/Login';
+import Login from './features/auth/Login';
 import StudentProfile from "./features/student/profile/PortfolioProfile";
 import CoursePage from './features/student/pages/CoursePage';
 import LearningGoals from './features/student/course/goal/Goal';
